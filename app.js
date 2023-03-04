a073b2afad6ebd559b5ea84f1559c8a778454f84
crispynoodlesoup <thepanman321@gmail.com>
2023-03-04 04:19:41 -0600

    group colors by column for aesthetic

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const leftImageRow = document.querySelector(".image-scroll-left");
 const rightImageRow = document.querySelector(".image-scroll-right");
 
 const imageList = [
-  "IMG_7363.jpg",
-  "IMG_7364.jpg",
-  "IMG_7365.jpg",
   "IMG_7366.jpg",
-  "IMG_7367.jpg",
+  "IMG_7365.jpg",
+  "IMG_7374.jpg",
+  "IMG_7369.jpg",
+  "IMG_7370.jpg",
 ];
 
 const imageList2 = [
+  "IMG_7364.jpg",
+  "IMG_7363.jpg",
+  "IMG_7367.jpg",
   "IMG_7368.jpg",
-  "IMG_7369.jpg",
-  "IMG_7370.jpg",
   "IMG_7371.jpg",
   "IMG_7372.jpg",
-  "IMG_7374.jpg",
 ];
 
 imageList.forEach((name) => {
